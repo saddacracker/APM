5016e33ecf0e28d61dbf27766688b65c117c8278
Johnny Cardwell <cardwell.john@gmail.com>
2021-03-10 16:31:56 -0800

    add products to its own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,44 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail/product-detail.component';
-import { ProductDetailGuard } from './products/product-detail.guard';
 import { WelcomeComponent } from './home/welcome.component';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   // ours
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   // from angular
   imports: [
 
   BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { 
-        path: 'products/:id', 
-        canActivate: [ProductDetailGuard],
-        component: ProductDetailComponent 
-      },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' } // 404 Not Found
-    ], { useHash: true })
+    ], { useHash: true }),
+    ProductModule
   ],
   bootstrap: [ AppComponent ]
 })
---
+++ src/app/products/product.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router';
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { StarComponent } from '../shared/star.component';
+import { ProductDetailGuard } from './product-detail.guard';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      { 
+        path: 'products/:id', 
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent,
+      }
+    ])
+  ],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ConvertToSpacesPipe,
+    StarComponent
+  ]
+})
+export class ProductModule { }
